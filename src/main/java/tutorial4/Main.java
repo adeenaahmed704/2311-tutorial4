77d72ae7988292efc4bc949a8cd65e287e7f811e
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-03-14 18:32:49 -0400

    Comments out failed test

--- src/main/java/tutorial4/Main.java
+++ src/main/java/tutorial4/Main.java
@@ -3,11 +3,12 @@ package tutorial4;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		Thread.sleep(10000); // 10 seconds to attach debugger
+		//Thread.sleep(10000); // 10 seconds to attach debugger
 		int a = 10;
 		int b = 20;
 		int sum = add(a, b);
 		System.out.println("The sum is: " + sum);
+		//Thread.sleep(10000); // 10 seconds to attach debugger
 	}
 	public static int add(int a, int b) {
 		int s = a + b;
