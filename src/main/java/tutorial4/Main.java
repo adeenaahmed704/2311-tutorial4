fc286720badc2927aa527b21ea548a2c06ba91d9
Adeena Ahmed <yorkuni2@my.yorku.ca>
2026-03-14 18:15:55 -0400

    Updates files so debugging connects vm

--- src/main/java/tutorial4/Main.java
+++ src/main/java/tutorial4/Main.java
@@ -2,7 +2,8 @@ package tutorial4;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(10000); // 10 seconds to attach debugger
 		int a = 10;
 		int b = 20;
 		int sum = add(a, b);
